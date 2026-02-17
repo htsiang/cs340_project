43cc080bec95ecbcb08cc3a07d553c66dca42576
htsiang <htsiang3@gmail.com>
2026-02-17 15:39:03 -0800

    updating to match dependencies in deployed version

---
+++ backend/database/db-connector.js
@@ -0,0 +1,24 @@
+// citation for the following code
+// Date: 1/8/2026
+// Copied from (slightly modified for credentials):
+// Source URL: https://canvas.oregonstate.edu/courses/2031764/assignments/10323319?module_item_id=26243357
+
+import 'dotenv/config';
+
+// Get an instance of mysql we can use in the app
+let mysql = require('mysql2')
+
+// Create a 'connection pool' using the provided credentials
+const pool = mysql.createPool({
+    waitForConnections: true,
+    connectionLimit   : 10,
+    host              : process.env.HOST,
+    user              : process.env.USER,
+    password          : process.env.PASSWORD,
+    database          : process.env.MY_DB
+}).promise(); // This makes it so we can use async / await rather than callbacks
+
+// Export it for use in our application
+module.exports = pool;
+
+console.log(process.env.HOST)
