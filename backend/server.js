adeb91903442aa5f730b1ba2fb1dc0063ba4645d
htsiang <htsiang3@gmail.com>
2026-03-02 15:16:39 -0800

    typo in import.

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@
 // Source URL: https://canvas.oregonstate.edu/courses/2031764/assignments/10323319?module_item_id=26243357
 
 // Database
-import { pool as db } from './database/db-connector.js';
+import db from './database/db-connector.js';
 
 // Express
 import express from 'express';
