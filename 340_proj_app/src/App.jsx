60ed62746ad8d6451785aefde0db93ccc5dbab22
htsiang <htsiang3@gmail.com>
2026-03-02 14:13:47 -0800

    added backend url to frontend

--- 340_proj_app/src/App.jsx
+++ 340_proj_app/src/App.jsx
@@ -10,6 +10,12 @@ import SessionsHasTreatments from './pages/SessionsHasTreatments';
 import AddSessionsHasTreatments from './pages/AddSessionWithTreatments';
 import UpdateSessionWithTreatments from './pages/UpdateSessionWithTreatments';
 
+// Define the backend port and URL for API requests
+// code credit from class template
+// source URL: https://canvas.oregonstate.edu/courses/2031764/pages/exploration-web-application-technology-2?module_item_id=26243419
+const backendPort = 13139;  // Use the port you assigned to the backend server, this would normally go in a .env file
+const backendURL = `http://classwork.engr.oregonstate.edu:${backendPort}`;
+
 function App() {
   const [sessionToEdit, setSessionToEdit] = useState();
 
