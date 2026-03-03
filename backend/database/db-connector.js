b281b2607b4fb4e5e3759065bfa218b1782f0c60
htsiang <htsiang3@gmail.com>
2026-03-02 16:15:17 -0800

    change .env variable names

--- backend/database/db-connector.js
+++ backend/database/db-connector.js
@@ -12,9 +12,9 @@ import mysql from 'mysql2';
 const pool = mysql.createPool({
     waitForConnections: true,
     connectionLimit   : 10,
-    host              : process.env.HOST,
-    user              : process.env.USER,
-    password          : process.env.PASSWORD,
+    host              : process.env.DB_HOST,
+    user              : process.env.DB_USER,
+    password          : process.env.DB_PASSWORD,
     database          : process.env.MY_DB
 }).promise(); // This makes it so we can use async / await rather than callbacks
 
