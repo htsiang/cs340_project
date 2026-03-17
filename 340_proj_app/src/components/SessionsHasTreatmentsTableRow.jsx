56c8b3984713272624a97c22cb5c849eb58b67ef
htsiang <htsiang3@gmail.com>
2026-03-16 23:06:58 -0700

    added conversions for date and time columns

--- 340_proj_app/src/components/SessionsHasTreatmentsTableRow.jsx
+++ 340_proj_app/src/components/SessionsHasTreatmentsTableRow.jsx
@@ -2,6 +2,24 @@ import React from 'react';
 import '../App.css';
 
 function SessionsHasTreatmentsTableRow({ detail }){
+    const dateOnly = (date) => {
+        if (date.includes('T')) {
+            return date.split('T')[0];
+        } else {
+            return date;
+        }
+    }
+
+    const timeConverter = (date, time) => {
+        const newDate = new Date(`${dateOnly(date)}T${time}`);
+
+        return newDate.toLocaleTimeString('en-US', {
+            hour: 'numeric',
+            minute: '2-digit',
+            hour12: true
+        });
+    }
+
     return (
         <tr>
             <td className='columns'>{detail.sessionId}</td>
@@ -11,8 +29,8 @@ function SessionsHasTreatmentsTableRow({ detail }){
             <td className='columns'>{detail.nickname}</td>
             <td className='columns'>{detail.pokemonType}</td>
             <td className='columns'>{detail.species}</td>
-            <td className='columns'>{detail.dateCol}</td>
-            <td className='columns'>{detail.timeCol}</td>
+            <td className='columns'>{dateOnly(detail.dateCol)}</td>
+            <td className='columns'>{timeConverter(detail.timeCol)}</td>
             <td className='columns'>{detail.treatmentName}</td>
             <td className='columns'>{detail.treatmentDuration}</td>
         </tr>
--- 340_proj_app/src/components/SessionsTableRow.jsx
+++ 340_proj_app/src/components/SessionsTableRow.jsx
@@ -3,6 +3,24 @@ import '../App.css';
 import UpdateDelete from './UpdateDelete';
 
 function SessionsTableRow({ session, onDelete, onEdit }){
+    const dateOnly = (date) => {
+        if (date.includes('T')) {
+            return date.split('T')[0];
+        } else {
+            return date;
+        }
+    }
+
+    const timeConverter = (date, time) => {
+        const newDate = new Date(`${dateOnly(date)}T${time}`);
+
+        return newDate.toLocaleTimeString('en-US', {
+            hour: 'numeric',
+            minute: '2-digit',
+            hour12: true
+        });
+    }
+
     return (
         <tr>
             <td className='columns'>{session.sessionId}</td>
@@ -11,8 +29,8 @@ function SessionsTableRow({ session, onDelete, onEdit }){
             <td className='columns'>{session.lastName}</td>
             <td className='columns'>{session.pokemonType}</td>
             <td className='columns'>{session.species}</td>
-            <td className='columns'>{session.dateCol}</td>
-            <td className='columns'>{session.timeCol}</td>
+            <td className='columns'>{dateOnly(session.dateCol)}</td>
+            <td className='columns'>{timeConverter(session.timeCol)}</td>
             <td className='columns'>{session.cost}</td>
             <UpdateDelete className='columns' session={session} onDelete={onDelete} onEdit={onEdit}/>
         </tr>
